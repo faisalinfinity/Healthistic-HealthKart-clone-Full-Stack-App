8004d71282892ff1dbdadc066133592a682c9418
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-03-29 11:59:33 +0530

    added admin Addproduct

---
+++ frontend/src/admin/components/AddProducts.jsx
@@ -0,0 +1,244 @@
+import { Badge, Box, Button, Grid, Input, Select, useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "../../constants/constants";
+
+let schema = {
+  image: "",
+  title: "",
+  description: "",
+  price: "",
+  originalPrice: "",
+  sizes: "",
+  category: "",
+  rating: "",
+  review: "",
+  flavour: "",
+  brand: "",
+  tags: "",
+  stock: "",
+  adminId: "6422eaba43494981477f5a79",
+};
+
+export default function AddProduct() {
+  const [loading, setLoading] = useState(false);
+
+  const [product, setProduct] = useState(schema);
+  const toast = useToast();
+  const PostProductData = async (data) => {
+    setLoading(true);
+    let res = await axios({
+      method: "post",
+      url: BASE_URL + "/product",
+      data: data,
+    });
+
+    if (res.data) {
+      console.log(res.data);
+      toast({
+        title: `Products Added`,
+        description: "Products Added Successfully",
+        status: "success",
+        duration: 4000,
+        isClosable: true,
+      });
+
+      setLoading(false);
+    } else {
+      toast({
+        title: `Something Went Wrong`,
+        description: ` ${res.data.message} `,
+        status: "error",
+        duration: 4000,
+        isClosable: true,
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleInput = (e) => {
+    let text = e.target.value;
+
+    setProduct((prev) => {
+      return { ...prev, [e.target.name]: text };
+    });
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <Box padding={"8px 0px"}>
+      <Badge m={"8px"} colorScheme={"orange"}>
+        Add Single Product
+      </Badge>
+      <Grid
+        p={4}
+        gap={2}
+        gridTemplateColumns={{
+          lg: "repeat(3,1fr)",
+          sm: "repeat(3,1fr)",
+          base: "repeat(1,1fr)",
+        }}
+      >
+        <Input
+          placeholder="Image Links separated by comma (,) required"
+          name="image"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="Product Title required"
+          name="title"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="Product Description required"
+          name="description"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="Product Price required"
+          name="price"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="Product Original Price required"
+          name="originalPrice"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="sizes separated by comma (,) required"
+          name="sizes"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Select   name="category" onChange={handleInput}  placeholder="category required">
+  <option value='Vitamins'>Vitamins</option>
+  <option value='Ayurveda'>Ayurveda</option>
+  <option value='Nutrients'>Nutrients</option>
+  <option value='Food'>Food and Drinks</option>
+</Select>
+        {/* <Input
+         
+         
+         
+          border={"1px solid orange"}
+          color="orange"
+        ></Input> */}
+        <Input
+          placeholder="rating"
+          name="rating"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="review "
+          name="review"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="flavour"
+          name="flavour"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="brand required"
+          name="brand"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="tags (optional)"
+          name="tags"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+        <Input
+          placeholder="stock (required)"
+          name="stock"
+          onChange={handleInput}
+          border={"1px solid orange"}
+          color="orange"
+        ></Input>
+      </Grid>
+
+      <Button
+        colorScheme={"orange"}
+        isLoading={loading}
+        onClick={() => {
+          let {
+            image,
+            title,
+            description,
+            price,
+            originalPrice,
+            sizes,
+            category,
+            rating,
+            review,
+            flavour,
+            brand,
+            tags,
+            stock,
+          } = product;
+
+          product.image = product.image?.split(",");
+          product.sizes = product.sizes?.split(",");
+          product.price = +price;
+          product.originalPrice = +originalPrice;
+          product.rating = +rating;
+          product.review = +review;
+          product.stock = +stock;
+
+          console.log(product);
+
+          if (
+            title == "" ||
+            image == "" ||
+            description == "" ||
+            price == "" ||
+            originalPrice == "" ||
+            sizes == "" ||
+            category == "" ||
+            rating == "" ||
+            flavour == "" ||
+            brand == "" ||
+            review == "" ||
+            stock == "" ||
+            tags == ""
+          ) {
+            toast({
+              title: `Required details missing`,
+              description: "Please enter all required details",
+              status: "error",
+              duration: 4000,
+              isClosable: true,
+            });
+          } else {
+            PostProductData({ ...product });
+          }
+        }}
+      >
+        Add Product
+      </Button>
+    </Box>
+  );
+}
