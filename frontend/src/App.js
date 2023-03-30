f4ab1a8b3ea72e5f6f1b043f163d82f45eb76355
Vivek377 <vk640890@gmail.com>
2023-03-30 22:25:51 +0530

    single product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import AddProduct from "./admin/components/AddProducts";
 
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import SingleProductPage from "./pages/SingleProductPage";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Footer />
+      {/* <Navbar /> */}
+      {/* <Footer /> */}
+      <SingleProductPage />
     </>
   );
 }
