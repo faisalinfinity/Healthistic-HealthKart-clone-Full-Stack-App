bcde69cb9c2677657cc590b642bf5357b782cfd4
faisalinfinity <mujtabafaisal944.fk@gmail.com>
2023-04-02 15:57:45 +0530

    updated API URL everywhere

--- frontend/src/redux/CheckoutReducer/action.js
+++ frontend/src/redux/CheckoutReducer/action.js
@@ -26,8 +26,8 @@ const addToOrder = (data) => (dispatch) => {
     dispatch(orderLoading())
     return axios.post(`${BASE_URL}/users/order`, data, {
         headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NDI0M2I0NmVhN2FhMDU3YTI3ODI4YWUiLCJpYXQiOjE2ODAxNjQ3MDd9.dnwiGLzmb7tv-c6bKcIlGRmRNQsSz61NGRjcw1tNML8`,
-        }
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem("UserDetails")).token}`,
+          }
     }).then(res => localStorage.removeItem("newItem"))
         .catch(err => dispatch(orderError()))
 }
